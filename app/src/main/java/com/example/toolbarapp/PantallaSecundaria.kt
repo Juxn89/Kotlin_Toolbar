a43eb2c621593963456b63784ae7c3e40f6bb3d2
= <=>
2020-03-04 14:17:23 -0600

    Se agrega una nueva actividad vacía

---
+++ app/src/main/java/com/example/toolbarapp/PantallaSecundaria.kt
@@ -0,0 +1,12 @@
+package com.example.toolbarapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PantallaSecundaria : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_secundaria)
+    }
+}

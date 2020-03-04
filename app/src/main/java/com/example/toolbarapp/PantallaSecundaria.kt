6f87aebeec954d15500f8b18e5b1ee951babb7fd
= <=>
2020-03-04 14:53:25 -0600

    Se crea un menu para el toolbar en PantallaSecundaria con su icono y se implementa la navegación hacia atrás

--- app/src/main/java/com/example/toolbarapp/PantallaSecundaria.kt
+++ app/src/main/java/com/example/toolbarapp/PantallaSecundaria.kt
@@ -1,12 +1,29 @@
 package com.example.toolbarapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.widget.Button
+import androidx.appcompat.widget.Toolbar
 
 class PantallaSecundaria : AppCompatActivity() {
 
+    var toolbar:Toolbar? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_secundaria)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar_secundario)
+        toolbar?.setTitle(R.string.app_name)
+
+        setSupportActionBar(toolbar)
+        var actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_secundario, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }

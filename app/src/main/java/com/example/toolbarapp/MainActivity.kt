e9c869775cea0ebb4474a90f503ff50450d61251
= <=>
2020-03-04 13:48:18 -0600

    Se agrega el Id al toolbar creado y mediante código se establece como principal

--- app/src/main/java/com/example/toolbarapp/MainActivity.kt
+++ app/src/main/java/com/example/toolbarapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.toolbarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar = findViewById<Toolbar>(R.id.toolbar)
+        //toolbar?.title = "Titulo del toolbar"
+        toolbar?.setTitle(R.string.app_name)
+        setSupportActionBar(toolbar)
     }
 }

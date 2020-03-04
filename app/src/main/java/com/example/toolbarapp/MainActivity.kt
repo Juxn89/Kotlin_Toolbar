03e03066054e58c854d81c8eaac5daaa431d9ffd
= <=>
2020-03-04 14:18:26 -0600

    se agrega el menu creado al toolbar y se agrega eventos

--- app/src/main/java/com/example/toolbarapp/MainActivity.kt
+++ app/src/main/java/com/example/toolbarapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.toolbarapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +19,24 @@ class MainActivity : AppCompatActivity() {
         //toolbar?.title = "Titulo del toolbar"
         toolbar?.setTitle(R.string.app_name)
         setSupportActionBar(toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // onCreateOptionsMenu - cuándo se está creando las opciones
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Se agrega eventos a elemento seleccionado
+        when(item?.itemId){
+            R.id.bFav -> {
+                Toast.makeText(this, "Elemento añadido como favorito", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
     }
 }
